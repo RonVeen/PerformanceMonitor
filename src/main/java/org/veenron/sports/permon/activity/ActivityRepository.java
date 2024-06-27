be7f35f8f48fbdfc8136584381cbdb56c5335dcb
Ronveen <ronveen@gmail.com>
2024-06-27 19:34:17 +0200

    Sonarlint warnings

--- src/main/java/org/veenron/sports/permon/activity/ActivityRepository.java
+++ src/main/java/org/veenron/sports/permon/activity/ActivityRepository.java
@@ -2,7 +2,6 @@ package org.veenron.sports.permon.activity;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.veenron.sports.permon.athlete.Athlete;
 
 import java.util.Optional;
 
